e48a0323210b214f68b578985448da46bc361037
gheo <gheorghe.coanta@gmail.com>
2023-07-31 21:47:03 +0300

    006 Implement Exception Handling Task
    - add GlobalExceptionHandler

--- employee-service/src/main/java/com/gh/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/gh/employeeservice/EmployeeServiceApplication.java
@@ -3,7 +3,7 @@ package com.gh.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gh")
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
