f50f3010a2ee350361d1296d913f55df3ea517a5
Gheo <gheorghe.coanta@gmail.com>
2023-09-04 16:22:39 +0300

    010 Add Discovery Service
    - add Discovery Service

--- employee-service/src/main/java/com/gh/employeeservice/feing/DepartmentFeignClient.java
+++ employee-service/src/main/java/com/gh/employeeservice/feing/DepartmentFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "DEPARTMENT-SERVICE", url = "localhost:8080")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface DepartmentFeignClient {
 
     @GetMapping("/api/departments/{id}")
